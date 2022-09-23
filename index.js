64e6730052661e99e4092e3ec7f7f16186d1ef6f
fizfat <hafizhfattah99@gmail.com>
2022-09-23 21:52:13 +0700

    push

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ dotenv.config();
 const app = express();
 
 try {
-    await db.authenticate();
-    console.log('Database Connected...');
+  await db.authenticate();
+  console.log("Database Connected...");
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
 
-app.use(cors({ credentials:true, origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
-app.use('/products', productRoutes);
+app.use("/products", productRoutes);
 
-app.listen(5000, () => console.log('Server running at port 5000'));
+app.listen(5000, () => console.log("Server Running at port 5000"));
